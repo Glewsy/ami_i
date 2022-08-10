ab1c3ee8c45d425516b17b4df3a4aa059912c915
Glewsy <95069463+Glewsy@users.noreply.github.com>
2022-08-10 02:32:06 -0300

    animation

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 const Card = ({ pokeNumber, pokeball, setPokeNumber }) => {
 
-  
+
     function randompoke() {
         setPokeNumber(Math.floor(Math.random() * 890))
     }
@@ -16,8 +16,8 @@ const Card = ({ pokeNumber, pokeball, setPokeNumber }) => {
     useEffect(() => {
 
         (async function s() {
-        
-            if (pokeball.types[1] !== undefined ) {
+
+            if (pokeball.types[1] !== undefined) {
                 let colr = await pokeball.types[1].type.name + "s"
                 setSecondColorCard(colr)
             } else {
@@ -30,43 +30,45 @@ const Card = ({ pokeNumber, pokeball, setPokeNumber }) => {
 
     return (
         <div className={`pokeapp  `}>
-            <div className={
-                `poke_container
+            <div className="poke_card">
+                <div className={
+                    `poke_container
                      ${pokeball.types[0].type.name}
                     ${secondColorCard} }
-                     `
-            }>
-                
-                <div className='pokenumber'>
-                    {` N°${pokeball.id}`}
-                </div>
-                <div className={`colortwo `}>
-                    <img className='pokeimg' src={imgsrc} alt="loading" />
+                     `}>
 
-                    <div className='pokeinfo-container'>
-                        <p className="pokename">
-                            {pokeName}
-                        </p>
-                        <div className={`poketypes `}>
-                            Type :
-                            <div className={`typeone ${pokeball.types[0].type.name}`}> {` ${pokeball.types[0].type.name}  `}  </div>
-                            {pokeball.types[1] ? <div className={`typetwo ${pokeball.types[1].type.name}`}> {pokeball.types[1] ? ` ${pokeball.types[1].type.name}` : ""} </div> : ""}
-                        </div>
-                        <div className='abilities_container'>
-                            Abilities : {pokeball.abilities.length} {
-                                <div className="abilities">
-                                    <p className="ability">{`> ${pokeball.abilities[0].ability.name}`}</p>
-                                    <p className="ability">{pokeball.abilities[1] ? `>> ${pokeball.abilities[1].ability.name}` : ""}</p>
-                                    <p className="ability">{pokeball.abilities[2] ? `>>> ${pokeball.abilities[2].ability.name}` : ""}</p>
-                                    <p className="ability">{pokeball.abilities[3] ? `>>>> ${pokeball.abilities[3].ability.name}` : ""}</p>
-                                </div>}
-                        </div>
-                        <div className="pokedetails">
-                            <div className='detail1'>Height : {pokeball.height} Mts.</div>
-                            <div className='detail2'>Weight : {pokeball.weight} Kg.</div>
+                    <div className='pokenumber'>
+                        {` N°${pokeball.id}`}
+                    </div>
+                    <div className={`colortwo `}>
+                        <img className='pokeimg' src={imgsrc} alt="loading" />
+
+                        <div className='pokeinfo-container'>
+                            <p className="pokename">
+                                {pokeName}
+                            </p>
+                            <div className={`poketypes `}>
+                                Type :
+                                <div className={`typeone ${pokeball.types[0].type.name}`}> {` ${pokeball.types[0].type.name}  `}  </div>
+                                {pokeball.types[1] ? <div className={`typetwo ${pokeball.types[1].type.name}`}> {pokeball.types[1] ? ` ${pokeball.types[1].type.name}` : ""} </div> : ""}
+                            </div>
+                            <div className='abilities_container'>
+                                Abilities : {pokeball.abilities.length} {
+                                    <div className="abilities">
+                                        <p className="ability">{`> ${pokeball.abilities[0].ability.name}`}</p>
+                                        <p className="ability">{pokeball.abilities[1] ? `>> ${pokeball.abilities[1].ability.name}` : ""}</p>
+                                        <p className="ability">{pokeball.abilities[2] ? `>>> ${pokeball.abilities[2].ability.name}` : ""}</p>
+                                        <p className="ability">{pokeball.abilities[3] ? `>>>> ${pokeball.abilities[3].ability.name}` : ""}</p>
+                                    </div>}
+                            </div>
+                            <div className="pokedetails">
+                                <div className='detail1'>Height : {pokeball.height} Mts.</div>
+                                <div className='detail2'>Weight : {pokeball.weight} Kg.</div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className="back"></div>
             </div>
             <button
                 className='pokebutton'
